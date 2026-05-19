9de5fff7b9eb25bad981cd5e89d1c400e34841a2
“murarivamsharaj” <chmurari73@gmail.com>
2026-05-19 16:02:13 +0530

    “initial-commit”

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("E-Commerce Application");
+        System.out.println("E-Commerce   Application");
        /* CsvReader csvReader = new CsvReader();
         CustomerRepository customerRepository = new CustomerRepository(csvReader);
         System.out.println(customerRepository.getCustomers());*/
